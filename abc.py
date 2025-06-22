9118a72099a67badf772b3a35d889a81ed007603
Choi10 <80233053+ByHRG@users.noreply.github.com>
2025-06-22 17:51:07 +0900

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,80 +1,43 @@
-import requests
+import httpx
 import json
 
 
 class ABCMART:
-    def __init__(self):
-        self.url = "https://abcmart.a-rt.com/product/info?prdtNo="
 
     def url_setting(self, url):
         return url.split("&")[0].split("prdtNo=")[-1]
 
     def login(self, input):
-        url = "https://grandstage.a-rt.com/"
-        req = requests.get(url)
-        JSESSIONID = str(req.cookies).split("JSESSIONID=")[-1].split(" for")[0]
-        WMONID = str(req.cookies).split("WMONID=")[-1].split(" for")[0]
-        header = {
-            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) ApplewebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148APP_IOS_F",
-            "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "X-Requested-With": "XMLHttpRequest",
-            "Connection": "keep-alive",
-            "Cookie": f"JSESSIONID={JSESSIONID}; " f"WMONID={WMONID}",
-        }
-        url = "https://grandstage.a-rt.com/login/login-processing"
-        data = f'loginType=member&username={input["id"]}&password={input["pw"]}'
-        req = requests.post(url, data=data, headers=header)
-        UID = str(req.cookies).split("UID=")[-1].split(" for")[0]
-        header = {
-            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) ApplewebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148APP_IOS_F",
-            "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "X-Requested-With": "XMLHttpRequest",
-            "Connection": "keep-alive",
-            "Cookie": f"JSESSIONID={JSESSIONID}; " f"WMONID={WMONID}; " f"UID={UID}",
-        }
+        #로그인 처리는 비밀입니다.
         return header
 
-    def run(self, input):
-        product_code = input['url']
-        if "abcmart" in product_code:
-            product_code = self.url_setting(product_code)
-            header = self.login(input)
-            req = requests.get(self.url + product_code, headers=header)
-            output = {
-                "Name": req.json()["prdtName"],
-                "Model": req.json()["styleInfo"],
-                "Image": req.json()["productImage"][0]["imageUrl"],
-                "Price": str(req.json()["displayProductPrice"]),
-                "Url": "https://abcmart.a-rt.com/product/new?prdtNo="
-                + str(product_code),
-                "Stock": {},
-            }
-            for i in req.json()["productOption"]:
-                data = {str(i["optnName"]): str(i["orderPsbltQty"])}
-                output["Stock"].update(data)
-            return output
-        elif "grandstage.a-rt." in product_code:
-            product_code = self.url_setting(product_code)
-            header = self.login(input)
-            req = requests.get(self.url + product_code, headers=header)
-            output = {
-                "Name": req.json()["prdtName"],
-                "Model": req.json()["styleInfo"],
-                "Image": req.json()["productImage"][0]["imageUrl"],
-                "Price": str(req.json()["displayProductPrice"]),
-                "Url": "https://grandstage.a-rt.com/product/new?prdtNo="
-                + str(product_code),
-                "Stock": {},
-            }
+    def run(self, url):
+        product_code = self.url_setting(data['url'])
+        if "onthespot" in data['url']:
+            url = "https://www.onthespot.co.kr/"
+        elif "grandstage" in data['url']:
+            url = "https://grandstage.a-rt.com/"
+        else:
+            url = "https://abcmart.a-rt.com/"
+        output = {
+            "Url": f'{url}product?prdtNo={product_code}',
+            "Stock": {},
+        }
+        # url = f"{url}product/info?prdtNo=" + product_code
 
-            for i in req.json()["productOption"]:
-                data = {str(i["optnName"]): str(i["orderPsbltQty"])}
-                output["Stock"].update(data)
-            return output
+        header = self.login(data)
+        req = httpx.get(f"https://abcmart.a-rt.c비밀APINo={product_code}", headers=header).json()
+        size = req["productOption"][0]["optnName"]
+
+        url = f"{url}cart비밀API?prdtNo={product_code}&orderQty=0&mmnyPrdtYn=Y&optnAddAmt=0&clickTabId=All&layerType=Cart&dailyDlvyYn=N&prdtOptnNo={size}"
+        req = httpx.get(url, headers=header).json()["prdtOptList"]
+        for i in req:
+            data = {str(i["optnName"]): str(i["orderPsbltQty"])}
+            output["Stock"].update(data)
+        return output
+
 data = {
-    'url':'https://grandstage.a-rt.com/product/new?prdtNo=1020102173',
+    'url':'제품 URL',
     'id':'ABC계정',
     'pw': 'ABC암호'
 }
